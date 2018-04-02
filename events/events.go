914cf5c859304a8408841713df553cffa0cddb70
Bob Fanger <bfanger@gmail.com>
2018-04-02 16:26:13 +0200

    Implemented events

---
+++ events/events.go
@@ -0,0 +1,110 @@
+package events
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+var events chan sdl.Event
+var eventMutex sync.RWMutex
+var refresh bool
+var refreshMutex sync.Mutex
+
+var (
+	windowListeners []chan *sdl.WindowEvent
+)
+
+const (
+	refreshEvent int32 = 0 //int itoa
+)
+
+// Init creates the event channel and starts multiplexing the events
+func Init() {
+	events = make(chan sdl.Event)
+
+	go func() {
+		events := OnWindowEvents()
+		for e := range events {
+			if e.Event == sdl.WINDOWEVENT_EXPOSED {
+				Refresh()
+			}
+		}
+	}()
+
+	go func() {
+		for e := range events {
+			switch t := e.(type) {
+			case *sdl.QuitEvent:
+			case *sdl.WindowEvent:
+				eventMutex.RLock()
+				for _, c := range windowListeners {
+					select {
+					case c <- t:
+					default:
+						fmt.Println("blocked on WindowEvent")
+						c <- t
+						fmt.Println("resumed")
+					}
+				}
+				eventMutex.RUnlock()
+			default:
+				// fmt.Printf("%T%+v\n", e, e)
+			}
+		}
+	}()
+}
+
+// Quit closes the event stream
+func Quit() {
+	// @todo Remove eventListeners
+	close(events)
+}
+
+// Refresh marks the a
+func Refresh() {
+	refreshMutex.Lock()
+	defer refreshMutex.Unlock()
+	if refresh == false {
+		e := &sdl.UserEvent{
+			Type:      sdl.USEREVENT,
+			Timestamp: sdl.GetTicks(),
+			WindowID:  0,
+			Code:      refreshEvent}
+
+		sdl.PushEvent(e)
+	}
+	refresh = true
+}
+
+// OnWindowEvents start listening to window events
+func OnWindowEvents() chan *sdl.WindowEvent {
+	eventMutex.Lock()
+	defer eventMutex.Unlock()
+	c := make(chan *sdl.WindowEvent)
+	windowListeners = append(windowListeners, c)
+	return c
+}
+
+// EventLoop start the main event loop and keep running until a quit event
+func EventLoop(render chan bool) error {
+	if events == nil {
+		return fmt.Errorf("events not initialized")
+	}
+	var e sdl.Event
+	for {
+		refreshMutex.Lock()
+		if refresh {
+			render <- true
+			refresh = false
+		}
+		refreshMutex.Unlock()
+		e = sdl.WaitEvent()
+		switch e.(type) {
+		case *sdl.QuitEvent:
+			return nil
+		}
+		events <- e
+	}
+}
--- main.go
+++ main.go
@@ -2,80 +2,119 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strconv"
+	"sync"
+	"time"
 
 	"github.com/bfanger/clock/display"
+	"github.com/bfanger/clock/events"
 	"github.com/veandco/go-sdl2/sdl"
 	"github.com/veandco/go-sdl2/ttf"
 )
 
+var screenWidth, screenHeight int32 = 320, 240
+
 func main() {
+	defer fmt.Println("bye")
 	fmt.Print("Clock")
 
 	if err := sdl.Init(sdl.INIT_VIDEO); err != nil {
-		panic(err)
+		log.Fatalf("Couldn't initialize sdl: %v\n", err)
 	}
 	defer sdl.Quit()
 	if err := ttf.Init(); err != nil {
-		panic(err)
+		log.Fatalf("Couldn't initialize sdl_ttf: %v\n", err)
 	}
 	defer ttf.Quit()
-	window, err := sdl.CreateWindow("Clock", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		320, 240, sdl.WINDOW_SHOWN)
+	window, err := createWindow()
 	if err != nil {
-		panic(err)
+		log.Fatalf("Couldn't create window: %v\n", err)
 	}
 	defer window.Destroy()
 	r, err := display.NewRenderer(window)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Couldn't create renderer: %v\n", err)
 	}
 	image := display.NewImage("/Sites/clock3/src/github.com/bfanger/clock/assets/image.jpg")
 	defer image.Destroy()
 	r.Add(0, display.NewSprite("Background", image, 0, 0))
 
-	fmt.Print(" 3")
 	white := sdl.Color{R: 255, G: 255, B: 255, A: 255}
 	text := display.NewText("/Sites/clock3/src/github.com/bfanger/clock/assets/Roboto-Bold.ttf", 90, white, "23:99")
 	defer text.Destroy()
 	r.Add(2, display.NewSprite("Time", text, 50, 70))
-
-	first := true
-	quit := false
-	repaint := false
-	var event sdl.Event
-	for {
-		event = sdl.WaitEvent()
-		repaint, quit = handleEvent(event)
-
-		if quit {
-			return
-		}
-		if repaint || first {
+	var m sync.Mutex
+	render := make(chan bool)
+	defer close(render)
+	go func() {
+		for range render {
+			m.Lock()
+			fmt.Print(".")
 			if err = r.Render(); err != nil {
 				panic(err)
 			}
+			m.Unlock()
 		}
-		if first {
-			fmt.Println(".0")
-			first = false
+	}()
+
+	events.Init()
+	defer events.Quit()
+	fmt.Println(" 3.0")
+
+	go func() {
+		count := 0
+		for {
+			count++
+			m.Lock()
+			text.Text = strconv.Itoa(count)
+			m.Unlock()
+			time.Sleep(time.Second)
+			events.Refresh()
 		}
+
+	}()
+
+	if err := events.EventLoop(render); err != nil {
+		log.Fatalf("eventLoop: %v\n", err)
 	}
+
 }
 
-func handleEvent(event sdl.Event) (repaint bool, quit bool) {
-	switch e := event.(type) {
-	case *sdl.QuitEvent:
-		quit = true
-	case *sdl.TouchFingerEvent:
-	case *sdl.MouseMotionEvent:
-	case *sdl.KeyboardEvent:
-	case *sdl.WindowEvent:
-		if e.Event == sdl.WINDOWEVENT_EXPOSED {
-			repaint = true
-		}
-	default:
-		// fmt.Printf("%T%+v\n", event, event)
-		repaint = false
+// createWindow on the second screen, or in fullscreen mode when the windowsize matches the displaysize
+func createWindow() (*sdl.Window, error) {
+	n, err := sdl.GetNumVideoDisplays()
+	if err != nil {
+		return nil, err
 	}
-	return
+	d, err := sdl.GetCurrentDisplayMode(0)
+	if err != nil {
+		return nil, fmt.Errorf("can't read display mode: %v", err)
+	}
+	var x, y int32
+	var flags uint32
+	if n > 1 {
+		x, y = sdl.WINDOWPOS_CENTERED_MASK+1, sdl.WINDOWPOS_CENTERED_MASK+1
+	} else {
+		x, y = sdl.WINDOWPOS_CENTERED, sdl.WINDOWPOS_CENTERED
+		x = 0
+		y = d.H - screenHeight
+	}
+	if d.W == screenWidth {
+		flags += sdl.WINDOW_FULLSCREEN
+	}
+
+	sdl.ShowCursor(sdl.DISABLE)
+
+	return sdl.CreateWindow("Clock", x, y, screenWidth, screenHeight, flags)
+
+}
+
+// isRaspberryPi checks if the display size is 320x240
+func isRaspberryPi() bool {
+	d, err := sdl.GetCurrentDisplayMode(0)
+	if err != nil {
+		log.Fatalf("can't read display mode: %v\n", err)
+	}
+	return d.W == screenWidth && d.H == screenHeight
 }
