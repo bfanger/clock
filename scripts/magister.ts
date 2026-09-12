d86e4bf030af86c16616fad9116e73238c873f0d
Bob Fanger <bfanger@gmail.com>
2026-09-12 19:11:09 +0200

    feat: Added tracing & additional assertions

--- scripts/magister.ts
+++ scripts/magister.ts
@@ -2,8 +2,11 @@ import { chromium, expect, type Page } from "@playwright/test";
 import { question } from "readline-sync";
 import fs from "node:fs";
 
+const headless = true;
+const passkey = "clock";
 const port = process.env.PORT || "8888";
-const passkeyPath = ".magisterPasskey.json";
+const storageState = "./magisterState.json";
+const passkeyPath = `.magisterPasskey.${passkey}.json`;
 const hasPasskey = fs.existsSync(passkeyPath);
 const { context, close } = await setup();
 
@@ -24,18 +27,47 @@ try {
 await close();
 
 async function getNextItem() {
+  await context.tracing.start({ snapshots: true, screenshots: true });
   const page = await context.newPage();
-  await page.goto("https://sovozaanstad.magister.net/magister/", {
-    waitUntil: "load",
-  });
-  const url = page.url();
-  if (new URL(url).hostname === "accounts.magister.net") {
-    await authenticate(page);
-  }
-  const item = await nextItem(page);
 
-  await page.close();
-  return item;
+  let traceFile: string | undefined;
+  try {
+    const authenticated = await openMagister(page);
+    if (!authenticated) {
+      await authenticate(page);
+    }
+    const item = await nextItem(page);
+    await context.storageState({ path: storageState });
+    return item;
+  } catch (err) {
+    traceFile =
+      "trace" +
+      new Date().toISOString().replaceAll("-", "").substring(0, 8) +
+      ".zip";
+    throw err;
+  } finally {
+    await context.tracing.stop({ path: traceFile });
+    if (traceFile) {
+      console.log(`\n\nnpx playwright show-trace ${traceFile}\n\n`);
+    }
+    await page.close();
+  }
+}
+
+async function openMagister(page: Page) {
+  return await step("opening magister", async () => {
+    await page.goto("https://sovozaanstad.magister.net/magister/", {
+      waitUntil: "load",
+    });
+    return await Promise.race([
+      expect(page.locator("#menu-agenda"))
+        .toBeVisible()
+        .then(() => true),
+      expect(page.getByRole("heading", { name: "Vul je gebruikersnaam in" }))
+        .toBeVisible()
+        .then(() => false),
+    ]);
+  });
 }
 
 type Item = {
@@ -44,35 +76,36 @@ type Item = {
   subject: string;
 };
 async function nextItem(page: Page) {
-  console.log("Loading agenda...");
-  const promise = new Promise<any>((resolve) => {
-    page.on("response", async (res) => {
-      if (res.url().match(/afspraken/)) {
-        const body = await res.text();
-        resolve(JSON.parse(body));
-      }
+  const items: Item[] = await step("checking agenda", async () => {
+    const promise = new Promise<any>((resolve) => {
+      page.on("response", async (res) => {
+        if (res.url().match(/afspraken/)) {
+          const body = await res.text();
+          resolve(JSON.parse(body));
+        }
+      });
     });
+    await page.locator("#menu-agenda").click();
+    const data = await Promise.race([
+      promise,
+      expect(page.locator("sl-format-date").filter({ hasText: "ma" }))
+        .toBeVisible()
+        .then(
+          () => new Promise((resolve) => setTimeout(resolve, 5_000, false)),
+        ),
+    ]);
+    if (!data) {
+      throw new Error("Failed to intercept agenda request");
+    }
+    return data.Items.map(
+      (item: any) =>
+        ({
+          day: new Date(item.Start).getDate(),
+          start: new Date(item.Start).getTime(),
+          subject: item.Omschrijving,
+        }) as Item,
+    ).toSorted((a: Item, b: Item) => a.start - b.start);
   });
-  await page.goto(
-    "https://sovozaanstad.magister.net/magister/#/agenda/schoolweek",
-    { waitUntil: "load" },
-  );
-  const data = await Promise.race([
-    promise,
-    new Promise((resolve) => setTimeout(resolve, 10_000, false)),
-  ]);
-  if (!data) {
-    throw new Error("Failed to load agenda data");
-  }
-
-  const items: Item[] = data.Items.map(
-    (item: any) =>
-      ({
-        day: new Date(item.Start).getDate(),
-        start: new Date(item.Start).getTime(),
-        subject: item.Omschrijving,
-      }) as Item,
-  ).toSorted((a: Item, b: Item) => a.start - b.start);
 
   const days: Record<number, Item> = {};
   for (const item of items) {
@@ -90,59 +123,63 @@ async function nextItem(page: Page) {
 
 async function authenticate(page: Page): Promise<void> {
   if (!hasPasskey) {
-    await page.locator("#username").fill(question("Username: "));
-    await page.locator("#username_submit").click();
-    await page.locator("#use_password_button").click();
-    await expect(page.locator("#password")).toBeVisible();
-    const password = question("Password: ", { hideEchoBack: true, mask: "" });
-    await page.locator("#password").fill(password);
-    await page.locator("#password_submit").click();
-    console.log("Logging in...");
-    await page.waitForURL(/sovozaanstad.magister.net\/magister\//, {
-      waitUntil: "domcontentloaded",
-    });
-    console.log("Saving passkey...");
-    await page.goto("https://accounts.magister.net/profile/#/");
-    await page
-      .getByRole("button", { name: "Wachtwoordsleutel toevoegen" })
-      .click();
-    try {
-      await expect(
-        page.getByRole("textbox", { name: "Naam van je wachtwoordsleutel" }),
-      ).toBeVisible({ timeout: 5000 });
-    } catch (err) {
-      console.log("Password verification required?");
+    await step("login", async () => {
+      await page.locator("#username").fill(question("Username: "));
+      await page.locator("#username_submit").click();
+      await page.locator("#use_password_button").click();
+      await expect(page.locator("#password")).toBeVisible();
+      const password = question("Password: ", { hideEchoBack: true, mask: "" });
       await page.locator("#password").fill(password);
       await page.locator("#password_submit").click();
-    }
-    await page
-      .getByRole("textbox", { name: "Naam van je wachtwoordsleutel" })
-      .fill("Clock");
-    await page.getByTestId("create-passkey").click();
-    await expect(page.getByText("Wachtwoordsleutels")).toBeVisible();
+      await page.waitForURL(/sovozaanstad.magister.net\/magister\//, {
+        waitUntil: "domcontentloaded",
+      });
 
-    const [credential] = await context.credentials.get({
-      rpId: "accounts.magister.net",
+      await page.goto("https://accounts.magister.net/profile/#/");
+      await page
+        .getByRole("button", { name: "Wachtwoordsleutel toevoegen" })
+        .click();
+      try {
+        await expect(
+          page.getByRole("textbox", { name: "Naam van je wachtwoordsleutel" }),
+        ).toBeVisible({ timeout: 5000 });
+      } catch (err) {
+        console.log("Password verification required?");
+        await page.locator("#password").fill(password);
+        await page.locator("#password_submit").click();
+      }
+      await page
+        .getByRole("textbox", { name: "Naam van je wachtwoordsleutel" })
+        .fill(passkey);
+      await page.getByTestId("create-passkey").click();
+      await expect(page.getByText("Wachtwoordsleutels")).toBeVisible();
+
+      const [credential] = await context.credentials.get({
+        rpId: "accounts.magister.net",
+      });
+      fs.writeFileSync(passkeyPath, JSON.stringify(credential));
     });
-    fs.writeFileSync(passkeyPath, JSON.stringify(credential));
+    if ((await openMagister(page)) === false) {
+      throw new Error("Opening after authenticating failed");
+    }
   } else {
-    console.log("Log in with passkey");
-    await page.locator("#use_fido_link").click();
-    await page.waitForURL(
-      "https://sovozaanstad.magister.net/magister/#/vandaag",
-      { waitUntil: "commit" },
-    );
+    await step("passkey", async () => {
+      await page.locator("#use_fido_link").click();
+      await page.waitForURL(
+        "https://sovozaanstad.magister.net/magister/#/vandaag",
+        { waitUntil: "commit" },
+      );
+    });
   }
 }
 
 async function setup() {
-  const storageState = "./magisterState.json";
   if (!fs.existsSync(storageState)) {
     fs.writeFileSync(storageState, "{}");
     fs.chmodSync(storageState, 0o600);
   }
 
-  const browser = await chromium.launch();
+  const browser = await chromium.launch({ headless });
   const context = await browser.newContext({ storageState });
 
   if (hasPasskey) {
@@ -154,7 +191,6 @@ async function setup() {
   return {
     context,
     close: async () => {
-      await context.storageState({ path: storageState });
       await context.close();
       await browser.close();
     },
@@ -188,4 +224,19 @@ async function sleep(reason: string, ms: number) {
   });
   console.log(`Waiting for ${reason} (${hours}h ${minutes}m) at ${until}`);
   return new Promise((resolve) => setTimeout(resolve, Math.max(0, ms) + 1000));
+}
+
+async function step<T>(name: string, fn: () => Promise<T>) {
+  context.tracing.group(name);
+  try {
+    process.stdout.write(name + " ...");
+    const result = await fn();
+    process.stdout.write(" [ok] \n");
+    return result;
+  } catch (err) {
+    process.stdout.write(" [failed] \n");
+    throw err;
+  } finally {
+    context.tracing.groupEnd();
+  }
 }
